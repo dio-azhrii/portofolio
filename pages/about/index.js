65fe0c9a8f149f52333e6281cc28ca7f765297ed
dioX <102281729+dio-azhrii@users.noreply.github.com>
2022-06-20 14:27:15 +0700

    Add files via upload

---
+++ pages/about/index.js
@@ -0,0 +1,37 @@
+import Layouts from '@/components/Layouts';
+import { Skills } from '@/data/Skills';
+import { motion } from 'framer-motion';
+import { ContentAnimation, FadeAnimation } from '@/components/Animations';
+
+const About = () => {
+  return (
+    <Layouts pageTitle=" | About">
+      <section className="relative flex w-full flex-col items-center justify-evenly py-40 md:h-screen md:flex-row md:overflow-hidden">
+        <motion.span {...FadeAnimation} className="title-page">
+          ABOUT
+        </motion.span>
+
+        <motion.div {...ContentAnimation} className="relative order-2 flex w-3/4 flex-col items-end justify-end md:order-1 md:w-1/3">
+          <div className="my-5 mr-2 text-left font-semibold text-primary-light">My Skills</div>
+          <div className="flex w-4/5 flex-wrap justify-evenly">
+            {Skills.map((skill) => (
+              <div key={skill.id} className="skills-icon">
+                <svg className="p-2" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path d={skill.svg} />
+                </svg>
+              </div>
+            ))}
+          </div>
+        </motion.div>
+
+        <motion.div {...ContentAnimation} className="relative order-1 my-10 w-3/4 text-justify md:order-2 md:my-0 md:w-1/4">
+          <p className="text-md font-light text-primary-light">
+            {`Hello! My name is Dio Alfikih Azhari, you can call me Dio, DioX or whatever you want. I'm a Front-end Developer and a Network Engineer located in Bandar Lampung, lampung, Indonesia. As a Front-end Develover I used to code websites from stratch. I love to create simple yet beautiful websites with great user experience.`}
+          </p>
+        </motion.div>
+      </section>
+    </Layouts>
+  );
+};
+
+export default About;
