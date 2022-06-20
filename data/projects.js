431ee4fdc4338065b8f23a38606c1a96474696d4
dioX <102281729+dio-azhrii@users.noreply.github.com>
2022-06-20 14:20:45 +0700

    Add files via upload

---
+++ data/projects.js
@@ -0,0 +1,32 @@
+export const Projects = [
+  {
+    id: 2,
+    name: 'Weather App',
+    slug: 'weather-app',
+    img: 'weather-app',
+    demo: 'https://weather.mhdrijal.my.id',
+    sourceCode: 'https://github.com/euxzy/weather-app',
+    description:
+      'Suhu Daerah.',
+  },
+  {
+    id: 3,
+    name: 'Jadwal Sholat',
+    slug: 'jadwal-sholat',
+    img: 'jadwal-sholat',
+    demo: 'https://jadwal-sholat.mhdrijal.my.id',
+    sourceCode: 'https://github.com/euxzy/jadwal-sholat',
+    description:
+      'Jadwal Sholat.',
+  },
+  {
+    id: 5,
+    name: 'Coming Soon Project',
+    slug: 'coming-soon-project',
+    img: 'coming-soon',
+    demo: 'https://',
+    sourceCode: 'https://',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla alias accusantium commodi quia rerum reprehenderit similique, ipsa cupiditate vel voluptates sapiente, unde ducimus dignissimos saepe consequuntur possimus maxime voluptatibus laudantium.',
+  },
+];
